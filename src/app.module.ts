58b15f9ba9723e388acebbb60a1e1aa332348af1
Anirban Pal <63857297+AnirbanPal4341@users.noreply.github.com>
2023-02-09 13:02:38 +0530

    Add files via upload

---
+++ src/app.module.ts
@@ -0,0 +1,25 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { User } from './users/user.entity';
+import { UserModule } from './users/user.module';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'UIpl#@2122',
+      database: 'demo',
+      entities: [User],
+      synchronize: true,
+    }),
+    UserModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
